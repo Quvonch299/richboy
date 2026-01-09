9f51061078610b64bebf94f486e6e2c07a177429
Quvonch299 <muminovquvonchbek85@gmail.com>
2026-01-09 15:02:07 +0500

    s

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -18,7 +18,6 @@ export default function Navbar({ selectedCountry, setSelectedCountry }) {
     <nav className="max-w-7xl text-white relative z-20 pt-4 px-4 md:px-0 m-auto flex items-center justify-between">
     
 
-      {/* Logo */}
       <div>
         <Image
           src="/icon/logo.svg"
@@ -29,11 +28,9 @@ export default function Navbar({ selectedCountry, setSelectedCountry }) {
         />
       </div>
 
-      {/* Right Section */}
       <div className="flex gap-4 md:gap-6 items-center relative">
         <a href="" className="hidden md:inline">+971 58 590 7875</a>
 
-        {/* Country Selector */}
         <div className="relative">
           <button
             onClick={() => setOpen(!open)}
@@ -61,47 +58,12 @@ export default function Navbar({ selectedCountry, setSelectedCountry }) {
           )}
         </div>
 
-        {/* Language Selector */}
         <span className="hidden md:flex items-center gap-1 cursor-pointer">
           ENG <MdOutlineKeyboardArrowDown />
         </span>
       </div>
 
-      {/* Mobile Menu */}
-      {mobileMenu && (
-        <div className="absolute top-full left-0 w-full bg-[#161516] flex flex-col items-start p-4 gap-4 md:hidden z-30">
-          <a href="">Car List</a>
-          <a href="">Yacht list</a>
-          <a href="">Chauffeur</a>
-          <a href="">+971 58 590 7875</a>
-          <div className="relative w-full">
-            <button
-              onClick={() => setOpen(!open)}
-              className="flex items-center gap-1 cursor-pointer w-full justify-between"
-            >
-              {countries.find(c => c.key == selectedCountry)?.label}
-              <MdOutlineKeyboardArrowDown />
-            </button>
-            {open && (
-              <div className="absolute top-full mt-2 left-0 w-full bg-black/80 backdrop-blur-md rounded-lg overflow-hidden">
-                {countries.map(country => (
-                  <button
-                    key={country.key}
-                    onClick={() => {
-                      setSelectedCountry(country.key)
-                      setOpen(false)
-                      setMobileMenu(false)
-                    }}
-                    className="block w-full text-left px-4 py-2 hover:bg-white/10"
-                  >
-                    {country.label}
-                  </button>
-                ))}
-              </div>
-            )}
-          </div>
-        </div>
-      )}
+       
     </nav>
   )
 }
